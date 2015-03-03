675aa2089e145213899f8dd09b6f9c83cb1c62b0
Mitch French <Mitch.French@gmail.com>
2015-03-03 15:36:06 -0800

    Added support for names for monsters

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -15,6 +15,7 @@ class MonstersController < ApplicationController
   # GET /monsters/new
   def new
     @monster = Monster.new
+    @monster.monster_names.build
   end
 
   # GET /monsters/1/edit
@@ -26,13 +27,22 @@ class MonstersController < ApplicationController
   def create
     @monster = Monster.new(monster_params)
 
-    respond_to do |format|
-      if @monster.save
-        format.html { redirect_to @monster, notice: 'Monster was successfully created.' }
-        format.json { render :show, status: :created, location: @monster }
-      else
-        format.html { render :new }
-        format.json { render json: @monster.errors, status: :unprocessable_entity }
+    if params[:add_monster_name]
+      # add a new, empty name
+      @monster.monster_names.build
+      render :action => 'new'
+    elsif params[:remove_monster_name]
+      # do nothing, handled by the model
+      render :action => 'new'
+    else
+      respond_to do |format|
+	if @monster.save
+	  format.html { redirect_to @monster, notice: 'Monster was successfully created.' }
+	  format.json { render :show, status: :created, location: @monster }
+	else
+	  format.html { render :new }
+	  format.json { render json: @monster.errors, status: :unprocessable_entity }
+	end
       end
     end
   end
@@ -40,13 +50,35 @@ class MonstersController < ApplicationController
   # PATCH/PUT /monsters/1
   # PATCH/PUT /monsters/1.json
   def update
-    respond_to do |format|
-      if @monster.update(monster_params)
-        format.html { redirect_to @monster, notice: 'Monster was successfully updated.' }
-        format.json { render :show, status: :ok, location: @monster }
-      else
-        format.html { render :edit }
-        format.json { render json: @monster.errors, status: :unprocessable_entity }
+    @monster = Monster.find(params[:id])
+    if params[:add_monster_name ]
+      # rebuild the names that don't have id's
+      unless params[:monster][:monster_names_attributes].blank?
+	for attribute in params[:monster][:monster_names_attributes]
+	  @monster.monster_names.build(attribute.last.except(:_destroy)) unless attribute.last.has_key?(:id)
+	end
+      end
+      # and add one more
+      @monster.monster_names.build
+      render :action => 'edit'
+    elsif params[:remove_monster_name]
+      # collect all marked for delete
+      removed_names = params[:monster][:monster_names_attributes].collect { |i, att| att[:id] if (att[:id] && att[:_destroy].to_i == 1 ) }
+      # remove from db
+      MonsterName.delete(removed_names)
+      for attribute in params[:monster][:monster_names_attributes]
+	@monster.monster_names.build(attribute.last.except(:_destroy)) if (!attribute.last.has_key?(:id) && attribute.last[:_destroy].to_i == 0)
+      end
+      render :action => 'edit', :notice => "Aliases removed."
+    else
+      respond_to do |format|
+	if @monster.update(monster_params)
+	  format.html { redirect_to @monster, notice: 'Monster was successfully updated.' }
+	  format.json { render :show, status: :ok, location: @monster }
+	else
+	  format.html { render :edit }
+	  format.json { render json: @monster.errors, status: :unprocessable_entity }
+	end
       end
     end
   end
@@ -69,6 +101,10 @@ class MonstersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def monster_params
-      params.require(:monster).permit(:strength, :dexterity, :intelligence, :health, :hitPoints, :will, :perception, :fatigue, :speed, :sizeModifier, :height, :weight, :gear, :description, :notes, :dodge, :block)
+      params.require(:monster).permit(:strength, 
+                                      :dexterity, :intelligence, :health, :hitPoints, :will, 
+                                      :perception, :fatigue, :speed, :sizeModifier, :height, 
+                                      :weight, :gear, :description, :notes, :dodge, :block,
+                                      monster_names_attributes: [:id, :name, :primary, :description, :_destroy])
     end
 end
--- app/models/monster.rb
+++ app/models/monster.rb
@@ -5,6 +5,7 @@ class Monster < ActiveRecord::Base
   has_many :attacks
   has_many  :damage_resistances
   has_many  :movement_rates
-  has_many  :monster_names
-  accepts_nested_attributes_for :monster_names
+  has_many  :monster_names, dependent: :destroy
+  accepts_nested_attributes_for :monster_names, allow_destroy: true,
+      reject_if: lambda {|attributes| attributes['name'].blank? }
 end
