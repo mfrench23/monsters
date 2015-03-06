78280c3e3508a130aaf682b4fb4f8f95e1b06ec8
Mitch French <Mitch.French@gmail.com>
2015-03-06 00:05:03 -0800

    Improved show screen for monsters by organizing fields in a table. Added better sort for monster names.

--- app/models/monster_name.rb
+++ app/models/monster_name.rb
@@ -1,3 +1,12 @@
 class MonsterName < ActiveRecord::Base
   belongs_to :monster
+  
+  
+  def <=>(other)
+    return nil unless other.is_a?(MonsterName)
+    
+    (( self.primary ? 1 : 2 ) <=> ( other.primary ? 1 : 2 )).nonzero? ||
+	(self.name <=> other.name).nonzero? ||
+	0
+  end
 end
