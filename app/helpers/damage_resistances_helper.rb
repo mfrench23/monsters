12d0e873d24d671c66556a5a0e966af1fb9462ae
Mitch French <Mitch.French@gmail.com>
2015-04-02 06:57:01 -0700

    Pull location to_s out of damage resistance helper

--- app/helpers/damage_resistances_helper.rb
+++ app/helpers/damage_resistances_helper.rb
@@ -1,15 +1,9 @@
 module DamageResistancesHelper
   def simpleDR(dr)
-    location = ""
-    if(!dr.location.nil?)
-      location = dr.location.name + ": "
-    end
-
-    notes = ""
     if dr.notes.to_s.strip.length > 0
       notes = " (" + dr.notes + ")"
     end
 
-    location + dr.dr.to_s + notes
+    [dr.location.to_s, dr.dr.to_s].join(": ") + notes.to_s
   end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,8 @@ class Location < ActiveRecord::Base
   has_many  :damage_resistances, :dependent => :destroy
 
   validates :name, presence: true
+
+  def to_s
+    name
+  end
 end
