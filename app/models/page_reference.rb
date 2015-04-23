144e608c595e802e67fbab0fe5f9ef7fe85ca8ed
Mitchell French <mitch.french@gmail.com>
2015-04-22 23:01:57 -0700

    Fix bug with display of page ref to book with 0-length abbrev

--- app/models/page_reference.rb
+++ app/models/page_reference.rb
@@ -12,7 +12,7 @@ class PageReference < ActiveRecord::Base
     if book.nil?
       s = pg
     else
-      s = ( book.abbreviation.nil? ? book.name : book.abbreviation ) + " #{pg}"
+      s = ( (book.abbreviation.nil? or book.abbreviation.empty?) ? book.name : book.abbreviation ) + " #{pg}"
     end
     s.strip
   end
--- test/models/page_reference_test.rb
+++ test/models/page_reference_test.rb
@@ -7,11 +7,15 @@ class PageReferenceTest < ActiveSupport::TestCase
     @book = FactoryGirl.create(:book, abbreviation: "NPt")
     @two = FactoryGirl.create(:page_reference, book: @book, pages: "23-4")
     @three = FactoryGirl.create(:page_reference, book: nil, pages: "23-4")
+    
+    @bookNameOnly = FactoryGirl.create(:book, abbreviation: "")
+    @four = FactoryGirl.create(:page_reference, book: @bookNameOnly, pages: nil)
   end
 
   test "to_s" do
     assert_equal "Some Random Book", @one.to_s
     assert_equal "NPt p. 23-4", @two.to_s
     assert_equal "p. 23-4", @three.to_s
+    assert_equal "Some Random Book", @four.to_s
   end
 end
