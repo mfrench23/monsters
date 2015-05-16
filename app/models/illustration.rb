e6064afff0c4438d0e9248694cefbc08b8820d7e
Mitchell French <mitch.french@gmail.com>
2015-05-16 13:09:46 -0700

    Display thumbnails of illustrations on Monster's new/edit pages

--- app/models/illustration.rb
+++ app/models/illustration.rb
@@ -2,8 +2,7 @@ class Illustration < ActiveRecord::Base
   belongs_to :illustratable, :polymorphic => true
 
   has_attached_file :image,
-      :styles => { :medium => "300x300>", :small => "75x75>" },
-      :default_url => "/images/:style/missing.png"
+      :styles => { :medium => "300x300>", :small => "75x75>" }
   validates_attachment_presence :image
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates_attachment_size :image, :less_than => (0.5).megabytes
