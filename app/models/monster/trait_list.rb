d4999f41fe6d6987a587458b08adb46c0c17f564
Mitchell French <mitch.french@gmail.com>
2015-05-17 15:36:35 -0700

    Refactor trait_list for shorter methods

--- app/models/monster/trait_list.rb
+++ app/models/monster/trait_list.rb
@@ -1,10 +1,38 @@
 module Monster::TraitList
   def self.getList(value)
+    out = break_on_semicolons(value)
+    # convert string patterns into Trait objects
+    out.map! do |line|
+      string_to_trait(line)
+    end
+    out
+  end
+
+  private
+
+  def self.string_to_trait(line)
+    sline = line.strip
+    if m = sline.match(/^(.*) \((.*)\) ([0-9]*)$/) then
+      mt = MasterTrait.find_or_create_by(:name => m[1], :notes => m[2])
+      retval = Trait.new(:master_trait => mt, :level => m[3])
+    elsif m = sline.match(/^(.*) ([0-9][0-9]*)$/) then
+      mt = MasterTrait.find_or_create_by(:name => m[1], :notes => nil)
+      retval = Trait.new(:master_trait => mt, :level => m[2])
+    elsif m = sline.match(/^(.*) \((.*)\)$/) then
+      mt = MasterTrait.find_or_create_by(:name => m[1], :notes => m[2])
+      retval = Trait.new(:master_trait => mt)
+    else
+      mt = MasterTrait.find_or_create_by(:name => sline, :notes => nil)
+      retval = Trait.new(:master_trait => mt)
+    end
+    retval
+  end
+
+  # break on semicolons, ignoring semi's inside parenthetical phrases
+  def self.break_on_semicolons(value)
     paren = 0
     out = []
     value.delete!("\n")
-
-    # break on semicolons, ignoring semi's inside parenthetical phrases
     value.scan(/./) do |c|
       if c == "(" then
 	paren += 1
@@ -22,27 +50,8 @@ module Monster::TraitList
 	out << c
       end
     end
-
     str = out.join
     out = str.split("\n")
-
-    # convert string patterns into Trait objects
-    out.map! do |line|
-      line.strip!
-      if m = line.match(/^(.*) \((.*)\) ([0-9]*)$/) then
-	mt = MasterTrait.find_or_create_by(:name => m[1], :notes => m[2])
-	Trait.new(:master_trait => mt, :level => m[3])
-      elsif m = line.match(/^(.*) ([0-9][0-9]*)$/) then
-	mt = MasterTrait.find_or_create_by(:name => m[1], :notes => nil)
-	Trait.new(:master_trait => mt, :level => m[2])
-      elsif m = line.match(/^(.*) \((.*)\)$/) then
-	mt = MasterTrait.find_or_create_by(:name => m[1], :notes => m[2])
-	Trait.new(:master_trait => mt)
-      else
-	mt = MasterTrait.find_or_create_by(:name => line, :notes => nil)
-	Trait.new(:master_trait => mt)
-      end
-    end
     out
   end
 end
