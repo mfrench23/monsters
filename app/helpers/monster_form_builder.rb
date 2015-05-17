7b411af9c6dc5a9e58b3e901d60eb3f111ce1c4c
Mitchell French <mitch.french@gmail.com>
2015-05-17 14:26:56 -0700

    Rename form builder class to be more general

--- app/helpers/monster_form_builder.rb
+++ app/helpers/monster_form_builder.rb
@@ -1,4 +1,4 @@
-class ClassedLabelFormBuilder < ActionView::Helpers::FormBuilder
+class MonsterFormBuilder < ActionView::Helpers::FormBuilder
   def label(method, text = nil, options = {}, &block)
     options.merge!({ :class => "field-label" })
     super
--- config/initializers/set_default_form_builder.rb
+++ config/initializers/set_default_form_builder.rb
@@ -2,12 +2,12 @@ module ActionView
   module Helpers
     module FormHelper
       def form_for_with_custom(record, options = {}, &proc)
-        options[:builder] = ClassedLabelFormBuilder
+        options[:builder] = MonsterFormBuilder
         form_for_without_custom(record, options, &proc)
       end
 
       def fields_for_with_custom(record_name, record_object = nil, options = {}, &block)
-        options[:builder] = ClassedLabelFormBuilder
+        options[:builder] = MonsterFormBuilder
         fields_for_without_custom(record_name, record_object, options, &block)
       end
 
