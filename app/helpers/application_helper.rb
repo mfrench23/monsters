e0a11f9cedad250cc0811ee3a390e09eb934b3e8
Mitchell French <mitch.french@gmail.com>
2015-05-17 15:20:55 -0700

    Add helper to generate standard submenus for entities

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def entity_links(method, options = {})
+    plural = method.to_s.pluralize.downcase
+
+    itext = "List All " + plural.titleize
+    index_url = send(plural + "_path")
+    index_link = link_to itext.to_s, index_url
+
+    ntext = "New " + method.to_s.titleize
+    new_url = send("new_" + method.to_s.downcase + "_path")
+    new_link = link_to ntext.to_s, new_url
+
+    list_entries = content_tag("li", index_link ) +
+                     content_tag("li", new_link )
+    return content_tag("ul", list_entries, options)
+  end
 end
