7ef5c64c44c10eca3ad43507a94d138192813959
Mitchell French <mitch.french@gmail.com>
2015-05-20 14:31:33 -0700

    Simplify monster factory

--- test/factories/monsters.rb
+++ test/factories/monsters.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :monster do
     name "Tim the Test Monster"
-    association :monster_class
+    monster_class
     characteristic_monsters {[FactoryGirl.build(:characteristic_monster)]}
   end
 end
