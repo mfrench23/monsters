cd170ec168d184111f4c1a3c927b9304569ba80e
Mitchell French <mitch.french@gmail.com>
2015-05-21 14:38:09 -0700

    Refactor to simplify ApplicationHealper#entity_links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,22 @@
 module ApplicationHelper
   def entity_links(method, options = {})
     plural = method.to_s.pluralize.downcase
+    list_entries = content_tag("li", index_link(plural) ) +
+                     content_tag("li", new_link(method) )
+    return content_tag("ul", list_entries, options)
+  end
 
-    itext = "List All " + plural.titleize
-    index_url = send(plural + "_path")
+  private
+
+  def index_link(plural)
+    itext = "List All " + plural.to_s.titleize
+    index_url = send(plural.to_s + "_path")
     index_link = link_to itext.to_s, index_url
+  end
 
+  def new_link(method)
     ntext = "New " + method.to_s.titleize
     new_url = send("new_" + method.to_s.downcase + "_path")
     new_link = link_to ntext.to_s, new_url
-
-    list_entries = content_tag("li", index_link ) +
-                     content_tag("li", new_link )
-    return content_tag("ul", list_entries, options)
   end
 end
