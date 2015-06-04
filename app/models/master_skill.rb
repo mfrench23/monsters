7941acd44788f351f7608911b92839a788b28ab0
Mitchell French <mitch.french@gmail.com>
2015-06-03 20:29:16 -0700

    Nil out blank attributes

--- app/models/master_skill.rb
+++ app/models/master_skill.rb
@@ -1,5 +1,14 @@
 class MasterSkill < ActiveRecord::Base
   belongs_to :characteristic
+  before_validation :nil_blank_attributes
 
   validates :name, :characteristic, presence: true
+
+  private
+
+  def nil_blank_attributes
+    [:name, :notes].each do |attr|
+      self[attr] = nil if self[attr].blank?
+    end
+  end
 end
--- app/models/master_trait.rb
+++ app/models/master_trait.rb
@@ -1,8 +1,17 @@
 class MasterTrait < ActiveRecord::Base
   has_many :traits
   validates :name, presence: true
+  before_validation :nil_blank_attributes
 
   def to_s
     (is_feature ? "Feature: " : "") + name + (notes.nil? ? "" : " (" + notes + ")")
+  end
+
+  private
+
+  def nil_blank_attributes
+    [:name, :notes].each do |attr|
+      self[attr] = nil if self[attr].blank?
+    end
   end
 end
