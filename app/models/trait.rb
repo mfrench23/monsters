a5ce0e30fb8600abfafd1a55f21f4c601859f979
Mitchell French <mitch.french@gmail.com>
2015-06-16 16:35:08 -0700

    Sort monster list on master trait page

--- app/models/trait.rb
+++ app/models/trait.rb
@@ -2,8 +2,11 @@ class Trait < AbstractEntity
   belongs_to :creature, touch: true
   belongs_to :master_trait
 
+  has_one :monster, :through => :creature
+
   validates :master_trait, presence: true
 
+  delegate :name, to: :monster, prefix: true
   delegate :name, to: :master_trait, prefix: true
 
   def deep_copy
