624c60a485f459b17460803b0f1e4083fe87bc64
Mitchell French <mitch.french@gmail.com>
2015-08-04 14:25:48 -0700

    Set minimum length for EquipmentCategory name

--- app/models/equipment_category.rb
+++ app/models/equipment_category.rb
@@ -1,7 +1,10 @@
 # A general class of equipment, like "Weapons" or "Clothing".
 class EquipmentCategory < ActiveRecord::Base
   validates_uniqueness_of :name
+  validates :name, length: { minimum: 2 }
+
   has_many :equipment_types
+
   scope :order_by_name, -> { order(:name) }
 
   def to_s
--- test/models/equipment_category_test.rb
+++ test/models/equipment_category_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class EquipmentCategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "cannot save without a name of 2 or more characters" do
+    eq = EquipmentCategory.new(:name => nil)
+    assert_equal false, eq.save
+    eq.name = "A"
+    assert_equal false, eq.save
+    eq.name = "IT"
+    assert_equal true, eq.save
+  end
 end
