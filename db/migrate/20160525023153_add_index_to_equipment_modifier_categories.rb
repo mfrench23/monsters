57096293a956a6a42573d35a8d8bc41d5823b852
Mitchell French <mitch.french@gmail.com>
2016-05-24 21:19:35 -0700

    Clean up bad migration

--- db/migrate/20160525023153_add_index_to_equipment_modifier_categories.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToEquipmentModifierCategories < ActiveRecord::Migration
-  def change
-    add_index :equipment_modifier_categories, :equipment_modifier_category_id, name: 'eq_mod_cat_idx'
-  end
-end
