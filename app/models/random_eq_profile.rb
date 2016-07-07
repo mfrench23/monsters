cfbd01f3b74e23778f36198600182599fdd2f82e
Mitchell French <mitch.french@gmail.com>
2016-07-07 10:03:12 -0700

    Allow RandomEqProfiles signifying unmodified equipment

--- app/models/random_eq_profile.rb
+++ app/models/random_eq_profile.rb
@@ -6,6 +6,4 @@ class RandomEqProfile < ActiveRecord::Base
 
   has_many :random_eq_profile_line_items, dependent: :destroy
   accepts_nested_attributes_for :random_eq_profile_line_items, allow_destroy: true, :reject_if => lambda { |line_item| line_item[:quantity].blank? }
-
-  validates :random_eq_profile_line_items, presence: true
 end
