da2d3eca265160dc15c4b7a2ba3c7a46ea4a0024
Mitchell French <mitch.french@gmail.com>
2017-12-13 10:14:37 -0800

    Reducing parameters passed to ManyToManyCell

--- app/javascript/components/ManyToManyCell.jsx
+++ app/javascript/components/ManyToManyCell.jsx
@@ -6,12 +6,14 @@ import ManyToManyDisplayedField from "./ManyToManyDisplayedField.jsx"
 
 class ManyToManyCell extends React.Component {
   render () {
+    var isChecked = ! ( this.props.intersectionRow == undefined );
+    var intersectionRowId = ( isChecked ? this.props.intersectionRow.id.toString() : "" );
     return (
       <span>
-        <HiddenField identifier={this.props.cellId + "[id]"} val={this.props.intersection_row_id} />
+        <HiddenField identifier={this.props.cellId + "[id]"} val={intersectionRowId} />
         <HiddenField identifier={this.props.cellId + "[" + this.props.foreignKeyName + "]"} val={this.props.intersectedId} />
         <HiddenField identifier={this.props.cellId + "[_destroy]"} val="true" />
-        <ManyToManyCheckbox identifier={this.props.cellId + "[_destroy]"} val="false" checked={this.props.isChecked} />
+        <ManyToManyCheckbox identifier={this.props.cellId + "[_destroy]"} val="false" checked={isChecked} />
         <ManyToManyDisplayedField title={this.props.displayTitle} text={this.props.displayText} />
       </span>
     );
@@ -20,11 +22,10 @@ class ManyToManyCell extends React.Component {
 
 ManyToManyCell.propTypes = {
   cellId: PropTypes.string,
-  intersectionRowId: PropTypes.string,
   intersectedId: PropTypes.string,
-  isChecked: PropTypes.bool,
   foreignKeyName: PropTypes.string,
   displayTitle: PropTypes.string,
-  displayText: PropTypes.string
+  displayText: PropTypes.string,
+  intersectionRow: PropTypes.object
 };
 export default ManyToManyCell
