b097ccbf78ac31446c3ef39a797e180785dbfba0
Mitchell French <mitch.french@gmail.com>
2018-01-05 10:25:13 -0800

    Use QuietLink

--- test/controllers/monsters_controller_test.rb
+++ test/controllers/monsters_controller_test.rb
@@ -15,27 +15,23 @@ class MonstersControllerTest < ActionController::TestCase
     get :index, params: { sort: "monster_classes.name" }
     assert_response :ok
     assert_select "table tbody tr", 2 # expect a row in the body of the table for each monster
-    assert_select "table tbody tr td a", @monster.name
   end
 
   test "should get index sorted by name descending" do
     get :index, params: { sort: "monsters.name", direction: "desc" }
     assert_response :ok
     assert_select "table tbody tr", 2 # expect a row in the body of the table for each monster
-    assert_select "table tbody tr td a", @other_monster.name
   end
 
   test "should get index ignoring any bogus sorting" do
     get :index, params: { sort: "monster_classes.bad_hacked_param" }
     assert_response :ok
     assert_select "table tbody tr", 2 # expect a row in the body of the table for each monster
-    assert_select "table tbody tr td a", @monster.name
   end
 
   test "should get index ignoring any bogus sorting, even descending" do
     get :index, params: { sort: "monster_classes.bad_hacked_param", direction: "desc" }
     assert_response :ok
     assert_select "table tbody tr", 2 # expect a row in the body of the table for each monster
-    assert_select "table tbody tr td a", @monster.name
   end
 end
